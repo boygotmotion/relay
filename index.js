d1fdc0dc93ecd072acf0e7dcf18050b67d2faaab
boygotmotion <info.paypal.help.service@gmail.com>
2026-08-12 05:26:25 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const multer = require("multer");
 const Tesseract = require("tesseract.js");
+Tesseract.setCorePath("/tesseract-core-simd.wasm");
 const Jimp = require("jimp");
 
 const app = express();
